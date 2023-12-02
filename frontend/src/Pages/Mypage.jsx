80bd4718f7fdb9c85f8761e2cddf85a0b198d7d0
gudals123 <104682516+gudals123@users.noreply.github.com>
2023-12-03 05:37:51 +0900

    feat:#7 프로필 퍼블리싱

--- frontend/src/Pages/Mypage.jsx
+++ frontend/src/Pages/Mypage.jsx
@@ -2,13 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import Header_mypag from '../Components/Header_mypag';
+import Profile from '../Components/Profile';
+
+const MainWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width:100vw;
+    height:auto;
+    overflow: hidden;zoom: 1;position:relative;z-index:1;
+`;
+
+const FeedWrap = styled.div`
+    width:600px;
+    height:300px;
+    margin-top: 10px;
+    //border-left: 1px solid #ccc;
+    // /border-right: 1px solid #ccc;
+`;
 
 
 export default function Main() {
     return (
         <div>
             <Header_mypag/>
-            마이페이지
+            <MainWrap>
+                <FeedWrap>
+                    <Profile/>
+
+                </FeedWrap>
+            </MainWrap>
         </div>
         
     );
