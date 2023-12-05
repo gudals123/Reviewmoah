b1cd5a1b411dae338d2b61a4712dbe53006b121d
gudals123 <104682516+gudals123@users.noreply.github.com>
2023-12-05 15:02:55 +0900

    #17 회원가입페이지 디자인 수정

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -9,12 +9,13 @@ const MainWrap = styled.div`
     align-items: center;
     justify-content: center;
     width:100%;
-    height:90vh;
-    margin-top: 11px;
-    margin-bottom: 11px;
+    height:100%;
+
 `;
 
 const WrapContent = styled.div`
+    margin-top: 11px;
+    margin-bottom: 11px;
     width: 400px;
     height:auto;
     //position: absolute;
